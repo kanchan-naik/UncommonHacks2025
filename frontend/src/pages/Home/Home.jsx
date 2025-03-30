336e9efd5ae8289086049321468718a80cc58145
Kanchan Naik <kanchan@uchicago.edu>
2025-03-30 03:11:44 -0500

    added background image

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,6 @@
 import FaceDetection from "components/FaceDetection/FaceDetection";
 import { useState } from "react";
 import CameraService from "services/CameraService";
-import LightRow, { Direction } from "./components/LightRow";
 import Products from "./components/Products";
 import "./Home.css";
 
@@ -42,9 +41,8 @@ export default function Home() {
 
   return (
     <div id="main-page">
-      <LightRow id={"light-row-1"} count={3} direction={Direction.COLUMN} />
-      <LightRow id={"light-row-2"} count={5} direction={Direction.ROW} />
-      <LightRow id={"light-row-3"} count={3} direction={Direction.COLUMN} />
+      {/* Vanity background image */}
+      <div id="vanity-background" />
       <FaceDetection
         userSubmitted={userSubmitted}
         onScreenCapture={submitMakeupRequest}
